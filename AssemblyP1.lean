c958b84da97fe68ea287e1d16c216923db53aa00
Otto Jung <121908118+ottojung@users.noreply.github.com>
2026-09-25 19:53:08 -0700

    Mirror verified #91 AssemblyP1.lean

--- AssemblyP1.lean
+++ AssemblyP1.lean
@@ -6,6 +6,7 @@ import AssemblyP1.FixedLengthExactCounterexample
 import AssemblyP1.FixedLengthBinomialCounterexample
 import AssemblyP1.Section62BridgingCounterexample
 import AssemblyP1.SameLengthSection62Counterexample
+import AssemblyP1.Section62BidirectedFlow
 import AssemblyP1.FiniteSamplingCounterexample
 import AssemblyP1.PopulationReduction
 import AssemblyP1.OrientedRigidity
